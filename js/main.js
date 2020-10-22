1474ee3c4c7601a4e2972ef641a4613ddf811f6e
Dan Lopez <monsieur.crono@gmail.com>
2020-10-21 23:30:16 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 function lastUpdated(){
     var lastUpdateTimeStamp = document.lastModified;
-    document.getElementById("lastUpdate").innerHTML = lastUpdateTimeStamp;
 }
-
+document.getElementById("lastUpdate").innerHTML = lastUpdateTimeStamp;
 console.log(lastUpdated());
