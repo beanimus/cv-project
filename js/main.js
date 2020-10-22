a67fd5db75b6973ba09a6b2383333662b2a1da9c
beanimus <monsieur.crono@gmail.com>
2020-10-21 23:39:39 -0400

    update js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 function lastUpdated(){
     var lastUpdateTimeStamp = document.lastModified;
+    document.getElementById("lastUpdate").innerHTML = lastUpdateTimeStamp;
 }
-document.getElementById("lastUpdate").innerHTML = lastUpdateTimeStamp;
+
 console.log(lastUpdated());
